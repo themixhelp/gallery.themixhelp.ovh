57420bf2d7c4ad7ca707b85ebd6bdcbacd051b12
themixhelp <themixhelp@outlook.com>
2023-11-01 02:22:05 +0100

    fix typo, remove unnecessary placeholder images

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -6,6 +6,6 @@
 			<h1 class="mb-2 text-center text-6xl font-black uppercase">gallery</h1>
 		</RouterLink>
 
-		<p class="text-center font-medium">a little dose of my artwork.</p>
+		<p class="text-center font-medium">a little dose of my artwork</p>
 	</header>
 </template>
